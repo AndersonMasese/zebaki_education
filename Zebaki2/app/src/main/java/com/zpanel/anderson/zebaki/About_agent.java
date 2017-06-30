3c3ea424b18f4030f2de132bd45384423c1cc5c0
andersonmasese <andersonmasese@gmail.com>
2017-06-30 11:17:31 -0700

    first commit

---
+++ Zebaki2/app/src/main/java/com/zpanel/anderson/zebaki/About_agent.java
@@ -0,0 +1,50 @@
+package com.zpanel.anderson.zebaki;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class About_agent extends AppCompatActivity implements  View.OnClickListener{
+    public Button b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success);
+
+        b = (Button) findViewById(R.id.buttonx);
+        b.setOnClickListener(this);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+    @Override
+    public void onClick(View view) {
+        if(view==b)
+        {
+            //Intent intent=new Intent(About_agent.this,Professionals1.class);
+            //About_agent.this.startActivity(intent);
+            String highschools="schools";
+            Intent i=new Intent(About_agent.this,Profile1.class);
+            i.putExtra("universities",highschools);
+            startActivity(i);
+        }
+    }
+}
